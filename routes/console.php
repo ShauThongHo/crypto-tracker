5de94bcf64692f958c201bccb360c37ce2581358
ShauThongHo <161423722+ShauThongHo@users.noreply.github.com>
2026-03-22 19:20:51 +0800

    Use $this->comment in inspire command
    Fix a typo in routes/console.php: the 'inspire' Artisan command previously used '->comment(...)' which is invalid in the closure context. Change to '$this->comment(...)' so the command outputs the inspiring quote correctly.

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,5 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
-    ->comment(Inspiring::quote());
+    $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
